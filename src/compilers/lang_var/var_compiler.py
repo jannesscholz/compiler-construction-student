d41bdca46a9f07ba7ef8c5de49e0dc5b926e900c
JannesScholz <jscholz@stud.hs-offenburg.de>
2024-04-04 14:25:41 +0200

    final 1

--- src/compilers/lang_var/var_compiler.py
+++ src/compilers/lang_var/var_compiler.py
@@ -2,44 +2,79 @@ from lang_var.var_ast import *
 from common.wasm import *
 import lang_var.var_tychecker as var_tychecker
 from common.compilerSupport import *
-#import common.utils as utils
 
 def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
-    var_tychecker.tycheckModule(m)
-    wasm_instrs = compileStmts(m.stmts)
-    wasm_instrs: list[WasmInstr] = [WasmInstrConst('i64', 1), WasmInstrCall(WasmId('$print_i64'))]
-    main = WasmFunc(WasmId('$main'), [], None, [], wasm_instrs)
-    module = WasmModule(wasmImports(cfg.maxMemSize), [WasmExport('main', WasmExportFunc(WasmId('$main')))], [], [], WasmFuncTable([]), [main])
-    print(module)
+    vars = var_tychecker.tycheckModule(m)
+    wasm_instrs = compileStmts(m.stmts, vars)
+
+    locals: list[tuple[WasmId, WasmValtype]] = [(identToWasmId(x), 'i64') for x in vars]
+
+    module = WasmModule(wasmImports(cfg.maxMemSize),
+                        [WasmExport('main', WasmExportFunc(WasmId('$main')))],
+                        [],
+                        [],
+                        WasmFuncTable([]),
+                        [WasmFunc(WasmId('$main'), [], None, locals, wasm_instrs)])
     return module
 
-def compileStmts(stmts: list[stmt]) -> list[WasmInstr]:
+def compileStmts(stmts: list[stmt], vars: set[ident]) -> list[WasmInstr]:
     wasm_instrs: list[WasmInstr] = []
 
     for stmt in stmts:
         match stmt:
             case Assign():
-                wasm_instrs.extend(compileAssignStmt(stmt))
+                wasm_instrs.extend(compileAssignStmt(stmt, vars))
             case StmtExp():
-                wasm_instrs.extend(compileExpStmt(stmt))
+                wasm_instrs.extend(compileExpStmt(stmt, vars))
     return wasm_instrs
 
-def compileAssignStmt(stmt: Assign) -> list[WasmInstr]:
-    var_tychecker.tycheckStmt(stmt, set())
+def compileAssignStmt(stmt: Assign, vars: set[ident]) -> list[WasmInstr]:
+    var_tychecker.tycheckStmt(stmt, vars)
 
-    #stmt.var
-    #WasmInstr
+    wasm_instrs: list[WasmInstr] = []
+    wasm_instrs.extend(compileExp(stmt.right, vars))
+    wasm_instrs.append(WasmInstrVarLocal('set', identToWasmId(stmt.var)))
+    vars.add(stmt.var)
 
-    wasm_instrs = []
-    wasm_instrs.extend(compileIdent(stmt.var))
     return wasm_instrs
 
-def compileIdent(var: ident) -> list[WasmInstr]:
-    wasm_instrs = []
+def compileExpStmt(stmt: StmtExp, vars: set[ident]) -> list[WasmInstr]:
+    var_tychecker.tycheckStmt(stmt, vars)
+    wasm_instrs: list[WasmInstr] = []
+    wasm_instrs.extend(compileExp(stmt.exp, vars))
     return wasm_instrs
 
-def compileExpStmt(stmt: StmtExp) -> list[WasmInstr]:
-    var_tychecker.tycheckStmt(stmt, set())
-    wasm_instrs = []
+def compileExp(e: exp, vars: set[ident]) -> list[WasmInstr]:
+    var_tychecker.tycheckExp(e, vars)
+    wasm_instrs: list[WasmInstr] = []
     
+    match e:
+        case IntConst(v):
+            wasm_instrs.append(WasmInstrConst('i64', v))
+        case Call(id, args):
+            var_tychecker.tycheckFuncall(id, args, vars)
+            for arg in args:
+                wasm_instrs.extend(compileExp(arg, vars))
+            wasm_instrs.append(WasmInstrCall(WasmId(f'${id.name.split("_")[0]}_i64')))
+        case UnOp(USub(), sub):
+            wasm_instrs.extend(compileExp(sub, vars))
+            wasm_instrs.append(WasmInstrConst('i64', -1))
+            wasm_instrs.append(WasmInstrNumBinOp('i64', 'mul'))
+        case BinOp(left, op, right):
+            wasm_instrs.extend(compileExp(left, vars))
+            wasm_instrs.extend(compileExp(right, vars))
+            match op:
+                case Add():
+                    wasm_instrs.append(WasmInstrNumBinOp('i64', 'add'))
+                case Sub():
+                    wasm_instrs.append(WasmInstrNumBinOp('i64', 'sub'))
+                case Mul():
+                    wasm_instrs.append(WasmInstrNumBinOp('i64', 'mul'))
+            
+        case Name(ident):
+            wasm_instrs.append(WasmInstrVarLocal('get', identToWasmId(ident)))
+
     return wasm_instrs
+
+def identToWasmId(identifier: ident) -> WasmId:
+    return WasmId('$' + identifier.name)
